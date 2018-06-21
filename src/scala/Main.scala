3abd053f00b0b80c1c367408fd2d381b43a11f8a
Yesid Botero <yesidboterom@gmail.com>
2018-06-20 22:58:59 -0500

    Knowing Show typeclass

--- src/scala/Main.scala
+++ src/scala/Main.scala
@@ -2,6 +2,8 @@ import interfaces.Printer
 import interfaces.PrintableSyntax.PrintableOps
 import instances.PrintableInstances._
 import model.Cat
+import cats._
+import cats.implicits._
 
 object Main extends App {
   Printer.print(1)
@@ -14,5 +16,20 @@ object Main extends App {
   // Using interface syntax
 
   cat.print
+
+
+  // Using Show typeclass
+
+  val showInt: Show[Int] = Show.apply[Int]
+  showInt.show(2)
+
+  // using extension method for Int instance
+
+  val showIntAsString = 2.show
+  println(showIntAsString)
+
+
+
+
 
 }
